2548c9276db49912fc88f00db7130f48590dd5e0
Alina1818 <krukun18@gmail.com>
2026-06-10 14:30:10 +0300

    Create mart_daily_performance.sql

---
+++ models/marts/mart_daily_performance.sql
@@ -0,0 +1,60 @@
+{{ config(materialized='table') }}
+
+with sessions as (
+
+    select *
+    from {{ ref('int_sessions') }}
+
+)
+
+select
+
+    session_date,
+
+    channel_group,
+
+    source,
+    medium,
+
+    country,
+    city,
+
+    device_category,
+    os,
+    browser,
+    
+
+    count(*) as sessions,
+
+    count(distinct user_pseudo_id) as users,
+
+    sum(transaction_count) as transactions,
+
+    sum(session_revenue) as revenue,
+
+    countif(converted) as converting_sessions,
+
+    round(
+        countif(converted)
+        / nullif(count(*),0),
+        4
+    ) as session_cvr,
+
+    round(
+        sum(session_revenue)
+        / nullif(sum(transaction_count),0),
+        2
+    ) as aov,
+
+    round(
+        sum(session_revenue)
+        / nullif(count(*),0),
+        2
+    ) as revenue_per_session,
+
+    countif(is_new_user) as new_users,
+    round(countif(is_new_user) / nullif(count(*),0), 4) as new_user_share
+
+from sessions
+
+group by all
